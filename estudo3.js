549b4e5195b0f61e4bca7150567a73a807ee1557
Kleber Harnebach Rodrigues <kleberharnebach@yahoo.com.br>
2022-12-05 11:15:02 -0300

    Delete estudo3.js

--- estudo3.js
+++
@@ -1,9 +0,0 @@
-const lista = document.getElementById("lista")
-
-function mostraTintas() {
-  if (lista.classList.contains("lista_show")) {
-    lista.classList.remove("lista_show")
-  } else {
-    lista.classList.add("lista_show")
-  }
-}
